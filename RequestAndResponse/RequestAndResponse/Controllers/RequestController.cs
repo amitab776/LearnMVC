5da4254dba3cc5c3360cb4183d4a1a4514019cec
Abhash Amit <amitab776@gmail.com>
2018-10-27 22:39:56 +0530

    Request And Response

---
+++ RequestAndResponse/RequestAndResponse/Controllers/RequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequestAndResponse.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public string Index()
+        {
+            string request = "";
+            request += "Url : " + Request.Url+"<br>";
+            request += "Browser :"+ Request.Browser.Type + "<br>";
+            request += "Application Path :"+ Request.ApplicationPath + "<br>";
+            request += "Path : "+Request.Path + "<br>";
+            request += "Http Method : "+Request.HttpMethod + "<br>";
+            request += "Query String : "+ Request.QueryString["x"] + "<br>";
+            request += "Headers : "+Request.Headers["User-Agent"] + "<br>";
+            return request;
+        }
+    }
+}
---
+++ RequestAndResponse/RequestAndResponse/Controllers/ResponseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequestAndResponse.Controllers
+{
+    public class ResponseController : Controller
+    {
+        // GET: Response
+        public void Index()
+        {
+            string response = "Response Object";
+            Response.ContentType = "text/html";
+            Response.StatusCode = 200;
+            Response.Write(response);
+        }
+    }
+}
