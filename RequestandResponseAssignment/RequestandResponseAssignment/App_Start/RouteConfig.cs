5b76a43a03e2f0c48244569348ca7f02b67ac949
Abhash Amit <amitab776@gmail.com>
2018-10-27 23:04:18 +0530

    Assignment Request And Response

---
+++ RequestandResponseAssignment/RequestandResponseAssignment/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RequestandResponseAssignment
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("HomeMaths","{controller}/{action}/{a}",new { },new { a = @"^[0-9+]$"});
+        }
+    }
+}
---
+++ RequestandResponseAssignment/RequestandResponseAssignment/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequestandResponseAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public void Index(int a)
+        {
+            Response.Write(Factorial(a));
+        }
+
+        private int Factorial(int a)
+        {
+            int factorialValue = 1;
+            for(int i = a; i > 0; i--)
+            {
+                factorialValue *= i;
+            }
+            return factorialValue;
+        }
+    }
+}
